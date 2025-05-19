e2114b3e4367d8c0ad8e5b9ee46429c31c0c27e2
JuliusMartensen <julius.martensen@gmail.com>
2025-05-19 16:28:11 +0200

    Fix unbound types in merge
    Needs test for type stability

--- lib/CorleoneCore/src/trajectory.jl
+++ lib/CorleoneCore/src/trajectory.jl
@@ -29,18 +29,21 @@ function Trajectory(sol::DESolution; mayer::Union{AbstractVector,Nothing}=nothin
     states = Array(sol)
     parameters = sol.prob.p
     time = sol.t
-    _mayer = isnothing(mayer) ? nothing : states[mayer,end]
+    _mayer = isnothing(mayer) ? nothing : states[mayer, end]
     Trajectory(states, parameters, time, nothing, sol.retcode == SciMLBase.ReturnCode.Success, mayer, _mayer)
 end
 
-function Base.merge(trajectories::Trajectory{U, P, T}...) where {U, P, T}
+function Base.merge(trajectories::Trajectory...)
     N = length(trajectories)
     states = reduce(hcat, map(i -> i == N ? trajectories[i].states : trajectories[i].states[:, 1:end-1], eachindex(trajectories)))
     time = reduce(vcat, map(i -> i == N ? trajectories[i].time : trajectories[i].time[1:end-1], eachindex(trajectories)))
     retcodes = all(x -> x.retcodes, trajectories)
     mayers = [x.mayer_variables for x in trajectories]
     mayer_indices = first(trajectories).mayer_indices
-    shooting_variables::Vector{NTuple{2, Vector{eltype(U)}}} = [(trajectories[i].states[.!mayer_indices, end], trajectories[i+1].states[.!mayer_indices, 1]) for i in  Base.OneTo(N - 1)]
-    Trajectory{U, P, T, typeof(shooting_variables), typeof(retcodes), typeof(mayer_indices),
-            typeof(mayers)}(states, trajectories[1].parameters, time, shooting_variables, retcodes, mayer_indices, mayers)
+    shooting_variables::Vector{NTuple{2,Vector{eltype(U)}}} = [(trajectories[i].states[.!mayer_indices, end], trajectories[i+1].states[.!mayer_indices, 1]) for i in Base.OneTo(N - 1)]
+    U = Base.promote_typeof(map(Base.Fix2(getfield, :states), trajectories)...)
+    P = typeof(first(trajectories).parameters)
+    T = Base.promote_typeof(map(Base.Fix2(getfield, :time), trajectories)...)
+    Trajectory{U,P,T,typeof(shooting_variables),typeof(retcodes),typeof(mayer_indices),
+        typeof(mayers)}(states, trajectories[1].parameters, time, shooting_variables, retcodes, mayer_indices, mayers)
 end
