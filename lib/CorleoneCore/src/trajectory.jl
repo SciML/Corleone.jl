4e3ebd614c3c88be486e989e30ad191911b215d7
Christoph Plate <christoph.plate@ovgu.de>
2025-05-20 12:27:14 +0000

    Some restructuring to make DirectControl work

--- lib/CorleoneCore/src/trajectory.jl
+++ lib/CorleoneCore/src/trajectory.jl
@@ -6,7 +6,7 @@ A container which collects the necessary information of a `DESolution`.
 # Fields
 $(FIELDS)
 """
-struct Trajectory{U,P,T,S,R,MI,M}
+struct Trajectory{U,P,T,S,R,SI,M}
     "The states of the trajectory"
     states::U
     "The parameters of the trajectory"
@@ -17,20 +17,21 @@ struct Trajectory{U,P,T,S,R,MI,M}
     shooting_variables::S
     "The returncodes"
     retcodes::R
-    "Mayer indices"
-    mayer_indices::MI
-    "Meyer variables"
+    "Indices of variables for pseudo-Mayer costs or controls."
+    special_variables::SI
+    "Evaluated pseudo-mayer costs on each shooting interval following
+        the transformation of the Lagrange term."
     mayer_variables::M
 end
 
 Base.eltype(x::Trajectory) = eltype(x.states)
 
-function Trajectory(sol::DESolution; mayer::Union{AbstractVector,Nothing}=nothing)
+function Trajectory(sol::DESolution; special_variables::Union{NamedTuple,Nothing}=nothing)
     states = Array(sol)
     parameters = sol.prob.p
     time = sol.t
-    _mayer = isnothing(mayer) ? nothing : states[mayer,end]
-    Trajectory(states, parameters, time, nothing, sol.retcode == SciMLBase.ReturnCode.Success, mayer, _mayer)
+    _mayer = isnothing(special_variables) ? nothing : states[special_variables.pseudo_mayer,end]
+    Trajectory(states, parameters, time, nothing, sol.retcode == SciMLBase.ReturnCode.Success, special_variables, _mayer)
 end
 
 function Base.merge(trajectories::Trajectory{U, P, T}...) where {U, P, T}
@@ -39,8 +40,9 @@ function Base.merge(trajectories::Trajectory{U, P, T}...) where {U, P, T}
     time = reduce(vcat, map(i -> i == N ? trajectories[i].time : trajectories[i].time[1:end-1], eachindex(trajectories)))
     retcodes = all(x -> x.retcodes, trajectories)
     mayers = [x.mayer_variables for x in trajectories]
-    mayer_indices = first(trajectories).mayer_indices
-    shooting_variables::Vector{NTuple{2, Vector{eltype(U)}}} = [(trajectories[i].states[.!mayer_indices, end], trajectories[i+1].states[.!mayer_indices, 1]) for i in  Base.OneTo(N - 1)]
-    Trajectory{U, P, T, typeof(shooting_variables), typeof(retcodes), typeof(mayer_indices),
-            typeof(mayers)}(states, trajectories[1].parameters, time, shooting_variables, retcodes, mayer_indices, mayers)
+    special_variables = first(trajectories).special_variables
+    shooting = .!(special_variables.pseudo_mayer .|| special_variables.control)
+    shooting_variables::Vector{NTuple{2, Vector{eltype(U)}}} = [(trajectories[i].states[shooting, end], trajectories[i+1].states[shooting, 1]) for i in  Base.OneTo(N - 1)]
+    Trajectory{U, P, T, typeof(shooting_variables), typeof(retcodes), typeof(special_variables),
+            typeof(mayers)}(states, trajectories[1].parameters, time, shooting_variables, retcodes, special_variables, mayers)
 end
