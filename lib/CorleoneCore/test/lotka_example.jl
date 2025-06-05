c00c5789299fc5ad5fb8fc9e7d41ce28a6459fb7
JuliusMartensen <julius.martensen@gmail.com>
2025-06-05 06:41:57 +0200

    Add Lotka Example for the current API

---
+++ lib/CorleoneCore/test/lotka_example.jl
@@ -0,0 +1,57 @@
+using CorleoneCore
+using ModelingToolkit
+using ModelingToolkit: t_nounits as t, D_nounits as D
+using OrdinaryDiffEq
+using Test
+
+# Lotka
+@variables begin 
+    x(..) = 0.5, [tunable=false] 
+    y(..) = 0.7, [tunable = false] 
+    u(..)=0.0 ,[input = true, bounds = (0., 1.)]
+end 
+@parameters begin 
+    p[1:4] = [1.0; 0.4; 1.0; 0.2], [tunable = false]
+end
+∫ = Symbolics.Integral(t in (0., 12.))
+
+@named lotka_volterra = System(
+    [
+        D(x(t)) ~ x(t) - p[1] * x(t) * y(t) - p[2] * x(t) * u(t),
+        D(y(t)) ~ -y(t) + p[3] * x(t) * y(t) - p[4] * y(t) * u(t)
+    ], t, [x(t), y(t), u(t)], [p];
+    costs=Num[∫((x(t) - 1)^2 + (y(t) - 1)^2)],
+    consolidate=(x...)->first(x)[1], # Hacky, IDK what this is at the moment
+)
+N = 48 
+shooting_points = [0.,3., 6.,9.]
+grid = ShootingGrid(shooting_points, DefaultsInitialization())
+controlmethod = DirectControlCallback(
+    u(t) => (; timepoints=collect(12 .* LinRange(0.,  N / (N+1), N)),
+        defaults= collect(LinRange(0., 1., N))  
+    )
+)
+builder = OCProblemBuilder(
+    lotka_volterra, controlmethod, grid, DefaultsInitialization()
+)
+
+# Instantiates the problem fully
+builder = builder()
+
+using SciMLSensitivity, Optimization, OptimizationMOI, Ipopt
+using SciMLSensitivity.ForwardDiff, SciMLSensitivity.Zygote, SciMLSensitivity.ReverseDiff
+
+optfun = OptimizationProblem{true}(builder, AutoForwardDiff(), Tsit5())
+
+# Initial plot
+sol = optfun.f.f.predictor(optfun.u0, saveat = 0.01)[1];
+plot(sol, idxs = [:x, :y, :u])
+
+# Optimize 
+sol = solve(optfun, Ipopt.Optimizer(); max_iter = 3000, tol = 1e-6, hessian_approximation="limited-memory", )
+
+# Result plot 
+pred = optfun.f.f.predictor(sol.u, saveat = 0.01)[1]
+f = plot(pred, idxs = [:x, :y, :u])
+
+
