acc5d5032c826f58ffb51c4dbb5cc125c5fd892f
JuliusMartensen <julius.martensen@gmail.com>
2025-09-02 12:33:04 +0200

    Add utils for merging and assignment

--- lib/CorleoneCore/src/local_controls.jl
+++ lib/CorleoneCore/src/local_controls.jl
@@ -11,7 +11,7 @@ end
 default_u(rng, t, bounds) = zeros(eltype(t), size(t))
 default_bounds(t::AbstractVector{T}) where T <: Real = (fill(typemin(T), size(t)), fill(typemax(T), size(t)))
 
-function ControlParameter(t::AbstractVector; name::Symbol = gensym(:w), controls = default_u, bounds = default_bounds(t))
+function ControlParameter(t::AbstractVector; name::Symbol = gensym(:w), controls = default_u, bounds = default_bounds)
     ControlParameter{typeof(t), typeof(controls), typeof(bounds)}(name, t, controls, bounds)
 end
 
@@ -25,10 +25,40 @@ function check_consistency(rng::Random.AbstractRNG, parameters::ControlParameter
     grid = get_timegrid(parameters)
     u = get_controls(rng, parameters)
     lb, ub = get_bounds(parameters)
-
     @assert issorted(grid) "Time grid is not sorted."
     @assert get_timegrid(parameters) == unique(grid) "Time grid is not unique."
     @assert all(lb .<= ub) "Bounds are inconsistent"
     @assert size(lb) == size(ub) == size(u) == size(grid) "Sizes are inconsistent"
     @assert all(lb .<= u .<= ub)  "Initial values are inconsistent"
+end
+
+function build_index_grid(controls::ControlParameter...; offset::Bool = true)
+    ts = map(get_timegrid, controls)
+    time_grid = reduce(vcat, ts) |> sort! |> unique!
+    indices = zeros(Int64, length(ts), size(time_grid, 1))
+    for i in axes(indices, 1), j in axes(indices, 2)
+        indices[i,j] = clamp(
+            searchsortedlast(ts[i], time_grid[j]), 
+            firstindex(ts[i]), lastindex(ts[i])
+        )
+    end
+    # Offset
+    if offset
+    for i in axes(indices, 1)
+        if i > 1
+            indices[i, :] .+= maximum(indices[i-1, :])
+        end
+    end
+    end
+    return indices
+end
+
+function collect_tspans(controls::ControlParameter...)
+    ts = map(get_timegrid, controls)
+    time_grid = reduce(vcat, ts) |> sort! |> unique!
+    tuple(collect(ti for ti in zip(time_grid[1:end-1], time_grid[2:end]))...)
+end
+
+function collect_local_controls(rng, controls::ControlParameter...)
+    reduce(vcat, map(vec ∘ Base.Fix1(get_controls, rng), controls))
 end
