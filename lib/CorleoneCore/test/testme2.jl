728f998446dce2fc19f8b65d13ff08b5d78c8bbd
JuliusMartensen <julius.martensen@gmail.com>
2025-09-02 14:09:43 +0200

    Adapted MWE for Single shooting

--- lib/CorleoneCore/test/testme2.jl
+++ lib/CorleoneCore/test/testme2.jl
@@ -2,74 +2,49 @@ using Pkg
 Pkg.activate(joinpath(pwd(), "lib/CorleoneCore"))
 
 using CorleoneCore
-
-using Pkg
-Pkg.activate(pwd())
-
-using OrdinaryDiffEqTsit5
+using OrdinaryDiffEq
 using SciMLSensitivity
+using ComponentArrays
 using LuxCore
 using Random
-using ComponentArrays
 
 using CairoMakie
 using BenchmarkTools
 using Zygote
-using SciMLSensitivity.ForwardDiff
+using ForwardDiff
 
-function lotka_dynamics(du, u, p, t, c)
-    du[1] = u[1] - prod(u) - 0.2 * c[1] * u[1]
-    du[2] = -u[2] + prod(u) - 0.4 * c[2] * u[2]
+using Optimization
+using OptimizationMOI
+using Ipopt
+
+function lotka_dynamics(du, u, p, t)
+    du[1] = u[1] - prod(u[1:2]) - 0.4 * p[1] * u[1]
+    du[2] = -u[2] + prod(u[1:2]) - 0.2 * p[1] * u[2]
     du[3] = (u[1]-1.0)^2 + (u[2] - 1.0)^2
 end
 
-
-tspans = ((0., 12.),)#(0., 6.), (nextfloat(6.), 12.))
-Δt = 0.1
-
-t0 = first(first(tspans))
-tinf = last(last(tspans))
-
-controls = SignalContainer(
-    PiecewiseConstant(
-        t0:Δt:(tinf-Δt)
-    ),
-    PiecewiseConstant(
-        t0:Δt:(tinf-Δt)
-    ),
-    aggregation=Base.Fix1(reduce, vcat)
-)
-
-dyn = ControlledDynamics(lotka_dynamics, controls)
-
-cdyn = CorleoneCore.wrap_model(Random.default_rng(), dyn)
-
+tspan = (0., 12.)
 u0 = [0.5, 0.7, 0.]
-tspan = (t0, tinf)
-p = ComponentArray(LuxCore.initialparameters(Random.default_rng(), dyn))
-problem = ODEProblem(cdyn, u0, tspan, p)
-layers = map(tspans) do tspani
-    (gensym(), ShootingProblem(problem, tspan=tspani, tunable = Int[]))
-end |> NamedTuple;
-
-shootingproblems = CorleoneCore.MultipleShootingProblem(layers)
-simlayer = CorleoneCore.SolverLayer(Tsit5(), EnsembleSerial(), shootingproblems)
-ps, st = LuxCore.setup(Random.default_rng(), simlayer)
-
+p0 = [0.0]
+prob = ODEProblem(lotka_dynamics, u0, tspan, p0, 
+    abstol = 1e-8, reltol = 1e-6, sensealg = ForwardDiffSensitivity()
+    )
+control = ControlParameter(
+    0.0:0.1:11.9, name = :fishing
+)
+layer = CorleoneCore.SingleShootingLayer(prob, Tsit5(),Int64[],[1], (control,))
+ps, st = LuxCore.setup(Random.default_rng(), layer)
 p = ComponentArray(ps)
 
-sol, st = simlayer(problem, ps, st)
-plot(sol)
-
-using Optimization, OptimizationMOI, Ipopt
-
-loss = let pax = ComponentArrays.getaxes(p), st = st, model = (p,st) -> simlayer(problem, p, st)
-    (p, ::Any) -> begin 
-        sol, _ =  model(ComponentArray(p, pax), st) 
-        xs = map(Array, sol)
-        last(xs)[end, end]#sum(sum(abs2, xi .- 1.0) for xi in xs)
+loss = let layer = layer, st = st, ax = getaxes(p)
+    (p, ::Any) -> begin
+        ps = ComponentArray(p, ax)
+        sols, _ = layer(nothing, ps, st)
+        Array(sols[end])[3, end]
     end
 end
+
+loss(p, nothing)
 
 loss(collect(p), nothing)
 
@@ -77,30 +52,52 @@ grad_fd = ForwardDiff.gradient(Base.Fix2(loss, nothing), collect(p))
 
 grad_zg = Zygote.gradient(Base.Fix2(loss, nothing), collect(p))[1]
 
+fig_grad = scatter(grad_fd)
+scatter!(grad_zg)
+display(fig_grad)
+
 optfun = OptimizationFunction(
-    loss, AutoZygote(), 
+    loss, AutoForwardDiff()
 )
 
 optprob = OptimizationProblem(
     optfun, collect(p), lb = zero(collect(p)), ub = zero(collect(p)) .+ 1
 )
 
-optimizer = Ipopt.Optimizer(
-)
+optimizer = Ipopt.Optimizer()
 
 uopt = solve(optprob, optimizer, 
-     hessian_approximation = "limited-memory", max_iter = 1000
+     #tol = 1e-6, 
+     #hessian_approximation = "limited-memory", 
+     max_iter = 200 # 165
 )
 
 
-popt = ComponentArray(uopt) + zero(p)
+t = LinRange(0., 11.9, length(uopt))
+fcontrols = stairs(t, uopt)
+display(fcontrols)
 
 sol, st = simlayer(problem, popt .+ zero(p), st)
+sol0, st = simlayer(problem, p, st)
+fsol = plot(sol)
 
-plot(sol)
+
+uopt = solve(optprob, Ipopt.Optimizer(), 
+     #tol = 1e-6, 
+     #hessian_approximation = "limited-memory", 
+     max_iter = 200 # 165
+)
+
+popt = ComponentArray(uopt) + zero(p)
+nopt = NamedTuple(popt)
+u1 = nopt.problems[1].p.controls.layers.layer_1.local_controls
+#u2 = nopt.problems[1].p.controls.layers.layer_2.local_controls
+t = LinRange(t0, tinf-Δt, length(u1))
+fcontrols = stairs(t, u1)
+#stairs!(t, u2)
+display(fcontrols)
 
 
-fff = scatter(grad_fd)
-
-scatter!(grad_zg)
-display(fff)
+sol, st = simlayer(problem, popt .+ zero(p), st)
+sol0, st = simlayer(problem, p, st)
+fsol = plot(sol)
