ade4f910e819985c6932987681c518c2b5d5ed10
Christoph Plate <christoph.plate@ovgu.de>
2025-10-02 11:18:47 +0200

    diels alder and DOW multiexperiment

--- lib/CorleoneCore/examples/dow_oed.jl
+++ lib/CorleoneCore/examples/dow_oed.jl
@@ -23,25 +23,23 @@ T₀ = 69 + 273.15
 R = 1.987204258640
 Q = 0.0131
 
-using OptimizationMOI.ModelingToolkit
 #######################
 # given starting values for the first four states
 #######################
-if temperature == 40
-    states_init = [1.7066, 8.32, 0.01, 0]
-elseif temperature == 67
-    states_init = [1.6749, 8.2262, 0.0104, 0.0017]
-else
-    states_init = [1.5608, 8.3546, 0.0082, 0.0086]
-end
 
-# rescale to Kelvin
-temperature += 273.15
+temperatures = [40., 67., 100.] # °C
+temperatures .+= 273.15 # K
+
+# Different temperatures have different initial conditions
+states_inits = [[1.7066, 8.32, 0.01, 0],
+                [1.6749, 8.2262, 0.0104, 0.0017],
+                [1.5608, 8.3546, 0.0082, 0.0086]]
+
+
 
 #######################
 # initial parameter estimates
 #######################
-
 p_init = [1.3978e12,3.1760e12,4.8880e28,1.847e4,1.882e4,2.636e4,1.4681e-17,6.4115e-7,1.e-17]
 
 function par_transform(p)
@@ -64,116 +62,28 @@ function par_transform(p)
     return new_p
 end
 
-u0 = vcat(states_init,
+
+u0s = map(states_inits) do states_init
+     vcat(states_init,
           0.0,
           0.0131,
-          0.5 * (-p_init[8] + sqrt(p_init[8]^2 + 4 * p_init[8] *states_init[1])),
+          0.5 * (-p_init[8] + sqrt(p_init[8]^2 + 4 * p_init[8] * states_init[1])),
           0.5 * (-p_init[8] + sqrt(p_init[8]^2 + 4 * p_init[8] * states_init[1])),
           0.0,
           0.0)
+end
 
-# select for each parameter whether it shall be estimated or fixed
+# Transform parameter values
 p_tranformed = par_transform(p_init)
-estimation_choice = [1, 0, 0, 1, 0, 0, 0, 0, 0]
-p_fix_init = []
-p_tune_init = []
-for i in eachindex(estimation_choice)
-    global p_tranformed, p_fix_init, p_tune_init
-    curr_par = p_tranformed[i]
-    if estimation_choice[i] == 1
-        p_tune_init = vcat(p_tune_init, curr_par)
-    else
-        p_fix_init = vcat(p_fix_init, curr_par)
-    end
-end
 
-tunable_start = false
-
-@variables t
-if tunable_start
-    @variables y₁(t) = states_init[1] [description = "Variable 1", tunable = true, bounds = (states_init[1] * 0.5, states_init[1] * 2)]
-    @variables y₂(t) = states_init[2] [description = "Variable 2", tunable = true, bounds = (states_init[2] * 0.5, states_init[2] * 2)]
-    @variables y₃(t) = states_init[3] [description = "Variable 3", tunable = true, bounds = (states_init[3] * 0.5, states_init[3] * 2)]
-    @variables y₄(t) = states_init[4] [description = "Variable 4", tunable = true, bounds = (states_init[4] * 0.5, states_init[4] * 2)]
-else
-    @variables y₁(t) = states_init[1] [description = "Variable 1"]
-    @variables y₂(t) = states_init[2] [description = "Variable 2"]
-    @variables y₃(t) = states_init[3] [description = "Variable 3"]
-    @variables y₄(t) = states_init[4] [description = "Variable 4"]
-end
-
-@variables y₅(t) = 0. [description = "Variable 5"]
-@variables y₆(t) = 0.0131 [description = "Variable 6"]
-@variables y₇(t) = 0.5 * (-p_init[8] + sqrt(p_init[8]^2 + 4*p_init[8]*states_init[1])) [description = "Variable 7"]
-@variables y₈(t) = 0.5 * (-p_init[8] + sqrt(p_init[8]^2 + 4*p_init[8]*states_init[1])) [description = "Variable 8"]
-@variables y₉(t) = 0. [description = "Variable 9"]
-@variables y₁₀(t) = 0. [description = "Variable 10"]
-
-@variables u(t) [description = "Control"]
-@parameters p_fix[1:length(p_fix_init)] = p_fix_init [description = "Fixed Parameters", tunable = false]
-@parameters p_tune[1:length(p_tune_init)] = p_tune_init [description = "Tunable Parameters", tunable = true]
-D = Differential(t)
-#@variables obs(t)[1:4] [description = "Observed", measurement_rate = 50]
-#obs = collect(obs)
-
-# summarize all parameters
-p_est = []
-tune_counter, fix_counter = 1, 1
-for i in eachindex(estimation_choice)
-    global p_fix, p_tune, p_est
-    global tune_counter, fix_counter
-    if estimation_choice[i] == 1
-        p_est = vcat(p_est, p_tune[tune_counter])
-        tune_counter += 1
-    else
-        p_est = vcat(p_est, p_fix[fix_counter])
-        fix_counter += 1
-    end
-end
-
-p_est
-
-print("p_est: ", p_est)
-
-# functions of tunable parameters
-k₁  = exp(p_est[1]) * exp(-p_est[4] * 1.e4/(R) * (1/temperature - 1/T₀))
-k₂  = exp(p_est[2]) * exp(-p_est[5] * 1.e4/(R) * (1/temperature - 1/T₀))
-k₋₁ = exp(p_est[3]) * exp(-p_est[6] * 1.e4/(R) * (1/temperature - 1/T₀))
-
-# abbreviation of ODE
-f₁ = -k₂ * y₈ * y₂
-f₂ = -k₁ * y₆ * y₂ + k₋₁ * y₁₀ - k₂ * y₈ * y₂
-f₃ = k₂ * y₈ * y₂ + k₁ * y₆ * y₄ - 0.5 * k₋₁ * y₉
-f₄ = -k₁ * y₆ * y₄ + 0.5 * k₋₁ * y₉
-f₅ = k₁ * y₆ * y₂ - k₋₁ * y₁₀
-f₆ = -k₁ * (y₆ * y₂ + y₆ * y₄) + k₋₁ * (y₁₀ + 0.5 * y₉)
-
-@named dow_oed = System(
-    [
-        D(y₁) ~  f₁;
-        D(y₂) ~  f₂;
-        D(y₃) ~  f₃;
-        D(y₄) ~  f₄;
-        D(y₅) ~  f₅;
-        D(y₆) ~  f₆;
-        0 ~ - y₇ - Q + y₆ + y₈ + y₉ + y₁₀;
-        0 ~ - y₈ + (exp(p_est[8]) * y₁) / (exp(p_est[8]) + y₇);
-        0 ~ - y₉ + (exp(p_est[9]) * y₃) / (exp(p_est[9]) + y₇);
-        0 ~ - y₁₀ + (exp(p_est[7]) * y₅) / (exp(p_est[7]) + y₇)
-    ], t, [y₁,y₂,y₃,y₄,y₅,y₆,y₇ ,y₈ ,y₉ ,y₁₀], vcat(p_tune, p_fix)
-)
-
-
-p = Symbolics.getdefaultval.(p_est)
-u0
-
-function dow(du, u, p_est, t)
+function dow(du, u, p, t)
 
     y₁,y₂,y₃,y₄,y₅,y₆,y₇ ,y₈ ,y₉ ,y₁₀ = u
     dy₁,dy₂,dy₃,dy₄,dy₅,dy₆,dy₇ ,dy₈ ,dy₉ ,dy₁₀ = du
-    k₁  = exp(p_est[1]) * exp(-p_est[4] * 1.e4/(R) * (1/temperature - 1/T₀))
-    k₂  = exp(p_est[2]) * exp(-p_est[5] * 1.e4/(R) * (1/temperature - 1/T₀))
-    k₋₁ = exp(p_est[3]) * exp(-p_est[6] * 1.e4/(R) * (1/temperature - 1/T₀))
+    temperature = p[10]
+    k₁  = exp(p[1]) * exp(-p[4] * 1.e4/(R) * (1/temperature - 1/T₀))
+    k₂  = exp(p[2]) * exp(-p[5] * 1.e4/(R) * (1/temperature - 1/T₀))
+    k₋₁ = exp(p[3]) * exp(-p[6] * 1.e4/(R) * (1/temperature - 1/T₀))
 
     # abbreviation of ODE
     f₁ = -k₂ * y₈ * y₂
@@ -191,37 +101,41 @@ function dow(du, u, p_est, t)
        -dy₅ +  f₅;
        -dy₆ +  f₆;
         - y₇ - Q + y₆ + y₈ + y₉ + y₁₀;
-        - y₈ + (exp(p_est[8]) * y₁) / (exp(p_est[8]) + y₇);
-        - y₉ + (exp(p_est[9]) * y₃) / (exp(p_est[9]) + y₇);
-        - y₁₀ + (exp(p_est[7]) * y₅) / (exp(p_est[7]) + y₇)
+        - y₈ + (exp(p[8]) * y₁) / (exp(p[8]) + y₇);
+        - y₉ + (exp(p[9]) * y₃) / (exp(p[9]) + y₇);
+        - y₁₀ + (exp(p[7]) * y₅) / (exp(p[7]) + y₇)
     ]
 end
 
 tspan = (0.,200.0)
 du0 = zeros(10)
-prob = DAEProblem{false}(dow,du0,u0, tspan, p,
-            #initializealg = NoInit(),
-            abstol=1e-8, reltol=1e-6,
-            sensealg = ForwardDiffSensitivity(),
-            differential_vars = [true, true, true, true, true, true, false, false, false, false]
-            )
+probs = map(zip(temperatures, u0s)) do (temperature, u0)
+    _prob = DAEProblem{false}(dow, du0, u0, tspan, vcat(p_tranformed, temperature),
+                #initializealg = NoInit(),
+                abstol=1e-8, reltol=1e-6,
+                sensealg = ForwardDiffSensitivity(),
+                differential_vars = [true, true, true, true, true, true, false, false, false, false]
+                )
 
-init_dae = init(prob, DFBDF())
+    init_dae = init(_prob, DFBDF())
 
-prob = DAEProblem{false}(dow,init_dae.du, init_dae.u, tspan, p,
-            initializealg = NoInit(),
-            abstol=1e-8, reltol=1e-6,
-            sensealg = ForwardDiffSensitivity(),
-            differential_vars = [true, true, true, true, true, true, false, false, false, false]
-            )
+    DAEProblem{false}(dow, init_dae.du, init_dae.u, tspan, _prob.p,
+                initializealg = NoInit(),
+                abstol=1e-8, reltol=1e-6,
+                sensealg = ForwardDiffSensitivity(),
+                differential_vars = [true, true, true, true, true, true, false, false, false, false]
+                )
+end
 
-sol = solve(prob, DFBDF())
-du0 = sol(0.0, Val{1})
-plot(sol)
+sols = [solve(probs[i], DFBDF()) for i=1:3]
+f = Figure()
+axs = [CairoMakie.Axis(f[i,1], title="Temperature: $(temperatures[i]) K") for i=1:3]
+[plot!(axs[i], sols[i]) for i=1:3]
+f
 
 observed = (u,p,t) -> u[1:4]
 
-layer = OEDLayer(prob, DFBDF(); params = [4,5,6], observed = observed, dt=1.0)
+layer = OEDLayer(probs[3], DFBDF(); params = [4,5,6], observed = observed, dt=1.0)
 
 ps, st = LuxCore.setup(Random.default_rng(), layer)
 pc = ComponentArray(ps)
@@ -243,21 +157,20 @@ end
 
 sampling_cons(zeros(4), collect(pc), nothing)
 
-@elapsed begin
-    optfun = OptimizationFunction(
-        ACrit, AutoReverseDiff(), cons = sampling_cons
-    )
+optfun = OptimizationFunction(
+    ACrit, AutoReverseDiff(), cons = sampling_cons
+)
 
-    optprob = OptimizationProblem(
-        optfun, collect(pc), lb = collect(lb), ub = collect(ub), lcons=zeros(4), ucons=40.0 * ones(4)
-    )
+optprob = OptimizationProblem(
+    optfun, collect(pc), lb = collect(lb), ub = collect(ub), lcons=zeros(4), ucons=40.0 * ones(4)
+)
 
-    uopt = solve(optprob, Ipopt.Optimizer(),
-        tol = 1e-9,
-        hessian_approximation = "limited-memory",
-        max_iter = 300
-    )
-end
+uopt = solve(optprob, Ipopt.Optimizer(),
+    tol = 1e-9,
+    hessian_approximation = "limited-memory",
+    max_iter = 300
+)
+
 optsol, _ = layer(nothing, uopt + zero(pc), st)
 
 f = Figure()
@@ -270,4 +183,79 @@ stairs!(ax3, 0.0:dt:last(tspan)-dt, (uopt + zero(pc)).controls[1:nc])
 stairs!(ax3, 0.0:dt:last(tspan)-dt, (uopt + zero(pc)).controls[nc+1:2*nc])
 stairs!(ax3, 0.0:dt:last(tspan)-dt, (uopt + zero(pc)).controls[2*nc+1:3*nc])
 stairs!(ax3, 0.0:dt:last(tspan)-dt, (uopt + zero(pc)).controls[3*nc+1:4*nc])
+f
+
+
+oedlayers = map(probs) do prob
+    OEDLayer(prob, DFBDF(); params = [4,5,6], observed = observed, dt=1.0)
+end
+
+multi_layer = CorleoneCore.MultiExperimentLayer(oedlayers...)
+#multi_layer = CorleoneCore.MultiExperimentLayer(oedlayers[1], 3)
+
+ps, st = LuxCore.setup(Random.default_rng(), multi_layer)
+ps_multi = ComponentArray(ps)
+lb_multi, ub_multi = CorleoneCore.get_bounds(multi_layer)
+
+crit = ACriterion()
+ACrit = crit(multi_layer)
+ACrit(ps_multi, st)
+
+sampling_cons_single = let nexp = multi_layer.n_exp, nc=ACrit.nc, st = st, dt = 1.0, ax = getaxes(ps_multi)
+    (res, p, ::Any) -> begin
+        ps = ComponentArray(p, ax)
+        samplings = reduce(vcat, map(1:nexp) do i
+            local_sampling = getproperty(ps, Symbol("experiment_$i"))
+            [sum(local_sampling.controls[nc[i][j]+1:nc[i][j+1]] * dt)  for j in eachindex(nc[i])[1:end-1]]
+        end)
+        res .= samplings
+    end
+end
+
+sampling_cons_multi = let nexp = multi_layer.n_exp, nc=ACrit.nc, st = st, dt = 1.0, ax = getaxes(ps_multi)
+    (res, p, ::Any) -> begin
+        ps = ComponentArray(p, ax)
+        samplings = reduce(vcat, map(1:nexp) do i
+            local_sampling = getproperty(ps, Symbol("experiment_$i"))
+            [sum(local_sampling.controls[nc[i][j]+1:nc[i][j+1]] * dt)  for j in eachindex(nc[i])[1:end-1]]
+        end)
+        res .= sum(samplings)
+    end
+end
+
+
+sampling_cons_single(zeros(12), ps_multi, st)
+sampling_cons_multi(zeros(1), ps_multi, st)
+
+optfun = OptimizationFunction(
+    ACrit, AutoReverseDiff(), cons = sampling_cons_multi
+)
+
+optprob = OptimizationProblem(
+    optfun, collect(ps_multi), lb = collect(lb_multi), ub = collect(ub_multi), lcons=[0.0], ucons=100 * ones(1)
+)
+
+uopt = solve(optprob, Ipopt.Optimizer(),
+    tol = 1e-9,
+    hessian_approximation = "limited-memory",
+    max_iter = 300
+)
+
+
+optsol, _ = multi_layer(nothing, uopt + zero(ps_multi), st)
+
+f = Figure(size = (800, 800))
+for i=1:3
+    ax = CairoMakie.Axis(f[1,i], xticks = 0:25:200, title="Experiment $i\nStates")
+    ax2 = CairoMakie.Axis(f[2,i], xticks = 0:25:200, title="Sensitivities")
+    ax3 = CairoMakie.Axis(f[3,i], limits=(nothing, (-0.05,1.05)), xticks = 0:25:200, title="Sampling")
+    [plot!(ax, optsol[i].t, sol) for sol in eachrow(Array(optsol[i]))[1:10]]
+    [plot!(ax2, optsol[i].t, sol) for sol in eachrow(reduce(hcat, (optsol[i][CorleoneCore.sensitivity_variables(multi_layer.layers[i])])))]
+    local_sampling = getproperty(uopt + zero(ps_multi), Symbol("experiment_$i"))
+
+    stairs!(ax3, 0.0:199, local_sampling.controls[1:200])
+    stairs!(ax3, 0.0:199, local_sampling.controls[201:400])
+    stairs!(ax3, 0.0:199, local_sampling.controls[401:600])
+    stairs!(ax3, 0.0:199, local_sampling.controls[601:end])
+end
 f
