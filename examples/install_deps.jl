0d53c7d7fcb8f6c412cac37bca3f90448ef10464
Christoph Plate <christoph.plate@ovgu.de>
2025-10-06 14:26:34 +0200

    moving stuff

--- examples/install_deps.jl
+++ examples/install_deps.jl
@@ -5,14 +5,12 @@ Pkg.activate(@__DIR__)
 if !isfile(joinpath(@__DIR__, "Project.toml"))
     # We add Corleone
     Pkg.add(url="https://kosinus.math.uni-magdeburg.de/mathopt/software/corleone", rev="main")
-    # We add all other deps 
-    Pkg.add("OrdinaryDiffEqTsit5")
-    Pkg.add("OrdinaryDiffEq")
-    Pkg.add("SciMLSensitivity")
-    Pkg.add("Optimization")
-    Pkg.add("OptimizationMOI")
-    Pkg.add("Ipopt")
-    Pkg.add("ModelingToolkit")
+    # We add all other deps
+    #Pkg.add("OrdinaryDiffEq")
+    #Pkg.add("SciMLSensitivity")
+    #Pkg.add("Optimization")
+    #Pkg.add("OptimizationMOI")
+    #Pkg.add("Ipopt")
     Pkg.add("CairoMakie")
 else
     # We add Corleone
