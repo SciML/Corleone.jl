79e2f714abbbac4fbeaedfef077d380950f0b31d
Christoph Plate <christoph.plate@ovgu.de>
2025-10-09 09:45:58 +0200

    testing local_controls

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,8 +11,8 @@ using SafeTestsets
     #@testset "Code linting (JET.jl)" begin
     #    JET.test_package(Corleone; target_defined_modules=true)
     #end
-    @testset "Optimal control" begin
-        include("oc.jl")
+    @testset "Local controls" begin
+        include("local_controls.jl")
     end
     @testset "OED augmentation" begin
         include("augmentation.jl")
@@ -20,22 +20,32 @@ using SafeTestsets
     @testset "Multiple shooting" begin
         include("multiple_shooting.jl")
     end
+    #@testset "Optimal control" begin
+    #    include("oc.jl")
+    #end
 end
 
 # What to test?
 # local_controls.jl:
-#   - different constructors + functions, bounds, timegrids and so on
 #   - construction of index_grid, get_subvector_indices -> Julius
 # node_initialization.jl:
 #   - for different layers (single / multiple shooting, OEDLayer, MultiExperimentLayer)
 # multi_experiments.jl:
 #   - constructors
 #   - inits, bounds, block structure
+#   - criteria
 # general:
 #   - prediction of layers: single shooting / multiple shooting / OED / MultiExperiment
 #   - convergence? Lotka OC + Lotka OED
+# oed.jl:
+#   - criteria
+#   - block_structure of oedlayer
+# augmentation.jl
+#   - all fixed
+#   - DAE augmentation + solve for sensitivities and check against FD
+#   - sensitivity_variables and observed_sensitivity_product_variables
 
-# using Coverage;
+#using Coverage;
 #coverage = process_folder();
 #coverage = merge_coverage_counts(coverage, filter!(
 #    let prefixes = (joinpath(pwd(), "src", ""),)
