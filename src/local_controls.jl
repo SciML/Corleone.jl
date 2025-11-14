8fe82a5008f913fa641c95f34c55e729a790e1b3
JuliusMartensen <julius.martensen@gmail.com>
2025-11-14 10:36:03 +0100

    Typestable Trajectory
    Adapt multiple shooting layer;
    Should also be consistent to duplicated controls

--- src/local_controls.jl
+++ src/local_controls.jl
@@ -5,19 +5,19 @@ Implements a piecewise constant control discretization.
 # Fields
 $(FIELDS)
 """
-struct ControlParameter{T, C, B}
-    "The name of the control"
-    name::Symbol
-    "The timepoints at which discretized variables are introduced"
-    t::T
-    "The initial values for the controls. Either a vector or a function (rng,t,bounds) -> u"
-    controls::C
-    "The bounds as a tuple"
-    bounds::B
+struct ControlParameter{T,C,B}
+  "The name of the control"
+  name::Symbol
+  "The timepoints at which discretized variables are introduced"
+  t::T
+  "The initial values for the controls. Either a vector or a function (rng,t,bounds) -> u"
+  controls::C
+  "The bounds as a tuple"
+  bounds::B
 end
 
 default_u(rng, t, bounds) = zeros(eltype(t), size(t))
-default_bounds(t::AbstractVector{T}) where T<:Real = (fill(typemin(T), size(t)), fill(typemax(T), size(t)))
+default_bounds(t::AbstractVector{T}) where {T<:Real} = (fill(typemin(T), size(t)), fill(typemax(T), size(t)))
 
 """
 $(METHODLIST)
@@ -47,16 +47,16 @@ ControlParameter{StepRangeLen{Float64, Base.TwicePrecision{Float64}, Base.TwiceP
 ```
 """
 function ControlParameter(t::AbstractVector; name::Symbol=gensym(:w), controls=default_u, bounds=default_bounds)
-    ControlParameter{typeof(t),typeof(controls),typeof(bounds)}(name, t, controls, bounds)
+  ControlParameter{typeof(t),typeof(controls),typeof(bounds)}(name, t, controls, bounds)
 end
 
 """
 Computes and returns a new tuple of `ControlParameter` with their timespans reduced to `(lo,hi)`.
 """
 function restrict_controls(c::Tuple, lo, hi)
-    map(c) do ci
-        restrict_controls(ci, lo, hi)
-    end
+  map(c) do ci
+    restrict_controls(ci, lo, hi)
+  end
 end
 
 """
@@ -64,115 +64,129 @@ Computes and returns a new `ControlParameter` from `c` with timepoints restricte
 `lo` .< `c.t` .< `hi`.
 """
 function restrict_controls(c::ControlParameter, lo, hi)
-    idx = findall(lo .<= c.t .< hi)
-    controls = c.controls == default_u ? c.controls : c.controls[idx]
-    bounds = c.bounds == default_bounds ? c.bounds : (length(c.bounds[1]) == 1 ? c.bounds : (c.bounds[1][idx], c.bounds[2][idx]))
-    return ControlParameter(copy(c.t[idx]), name = c.name, controls = controls, bounds = bounds)
+  idx = findall(lo .<= c.t .< hi)
+  controls = c.controls == default_u ? c.controls : c.controls[idx]
+  bounds = c.bounds == default_bounds ? c.bounds : (length(c.bounds[1]) == 1 ? c.bounds : (c.bounds[1][idx], c.bounds[2][idx]))
+  return ControlParameter(copy(c.t[idx]), name=c.name, controls=controls, bounds=bounds)
 end
 
 get_timegrid(parameters::ControlParameter) = collect(parameters.t)
-get_controls(::Random.AbstractRNG, parameters::ControlParameter{<:Any, <:AbstractArray}) = deepcopy(parameters.controls)
-get_controls(rng::Random.AbstractRNG, parameters::ControlParameter{<:Any, <:Function}) = parameters.controls(rng, parameters.t, parameters.bounds)
-get_bounds(parameters::ControlParameter{<:Any, <:Any, <:Tuple}) = begin
-    _bounds = getfield(parameters, :bounds)
-    nc = length(parameters.t)
-    if length(_bounds[1]) == length(_bounds[2]) == 1
-        return (repeat([_bounds[1]], nc), repeat([_bounds[2]], nc))
-    elseif length(_bounds[1]) == length(_bounds[2]) == nc
-        return _bounds
-    else
-        throw("Incompatible control bound definition. Got $(length(_bounds[1])) elements, expected $nc.")
-    end
+
+function get_controls(::Random.AbstractRNG, parameters::ControlParameter{<:Any,<:AbstractArray}; tspan = nothing, kwargs...)
+  (; t, controls) = parameters
+  idx = isnothing(tspan) ? eachindex(t) : findall(tspan[1] .<= t .< tspan[2])
+	controls[idx]
 end
-get_bounds(parameters::ControlParameter{<:Any, <:Any, <:Function}) = parameters.bounds(parameters.t)
+
+function get_controls(rng::Random.AbstractRNG, parameters::ControlParameter{<:Any,<:Function}; tspan=nothing, kwargs...)
+  (; t, bounds) = parameters
+  idx = isnothing(tspan) ? eachindex(t) : findall(tspan[1] .<= t .< tspan[2])
+  bounds = length(bounds[1]) == length(t) ? (bounds).([idx]) : bounds
+  parameters.controls(rng, t[idx], bounds)
+end
+
+get_bounds(parameters::ControlParameter{<:Any,<:Any,<:Tuple}) = begin
+  if length(_bounds[1]) == length(_bounds[2]) == 1
+    return (repeat([_bounds[1]], nc), repeat([_bounds[2]], nc))
+  elseif length(_bounds[1]) == length(_bounds[2]) == nc
+    return _bounds
+  else
+    throw("Incompatible control bound definition. Got $(length(_bounds[1])) elements, expected $nc.")
+  end
+end
+get_bounds(parameters::ControlParameter{<:Any,<:Any,<:Function}) = parameters.bounds(parameters.t)
 
 function check_consistency(rng::Random.AbstractRNG, parameters::ControlParameter)
-    grid = get_timegrid(parameters)
-    u = get_controls(rng, parameters)
-    lb, ub = get_bounds(parameters)
-    @assert issorted(grid) "Time grid is not sorted."
-    @assert get_timegrid(parameters) == unique(grid) "Time grid is not unique."
-    @assert all(lb .<= ub) "Bounds are inconsistent"
-    @assert size(lb) == size(ub) == size(u) == size(grid) "Sizes are inconsistent"
-    @assert all(lb .<= u .<= ub) "Initial values are inconsistent"
+  grid = get_timegrid(parameters)
+  u = get_controls(rng, parameters)
+  lb, ub = get_bounds(parameters)
+  @assert issorted(grid) "Time grid is not sorted."
+  @assert get_timegrid(parameters) == unique(grid) "Time grid is not unique."
+  @assert all(lb .<= ub) "Bounds are inconsistent"
+  @assert size(lb) == size(ub) == size(u) == size(grid) "Sizes are inconsistent"
+  @assert all(lb .<= u .<= ub) "Initial values are inconsistent"
 end
 
 function get_subvector_indices(M::Int, L::Int)
-    # Handle invalid inputs
-    if M < 0 || L <= 0
-        error("M must be a non-negative integer and L must be a positive integer.")
-    end
+  # Handle invalid inputs
+  if M < 0 || L <= 0
+    error("M must be a non-negative integer and L must be a positive integer.")
+  end
 
-    # Calculate the number of full-length vectors (N)
-    N = floor(Int, M / L)
+  # Calculate the number of full-length vectors (N)
+  N = floor(Int, M / L)
 
-    indices = Vector{UnitRange{Int64}}()
+  indices = Vector{UnitRange{Int64}}()
 
-    # Create the N full-length vectors
-    for i in 1:N
-        start_idx = (i - 1) * L + 1
-        end_idx = i * L
-        push!(indices, start_idx:end_idx)
-    end
+  # Create the N full-length vectors
+  for i in 1:N
+    start_idx = (i - 1) * L + 1
+    end_idx = i * L
+    push!(indices, start_idx:end_idx)
+  end
 
-    # Create the last vector with remaining elements
-    remaining_length = M - N * L
-    if remaining_length > 0
-        last_start_idx = N * L + 1
-        last_end_idx = M
-        push!(indices, last_start_idx:last_end_idx)
-    end
+  # Create the last vector with remaining elements
+  remaining_length = M - N * L
+  if remaining_length > 0
+    last_start_idx = N * L + 1
+    last_end_idx = M
+    push!(indices, last_start_idx:last_end_idx)
+  end
 
-    return indices
+  return indices
 end
 
-function build_index_grid(controls::ControlParameter...; offset::Bool=true, tspan::Tuple=(-Inf, Inf), subdivide::Int64 = typemax(Int64))
-    ts = map(controls) do ci
-        clamp.(get_timegrid(ci), tspan...)
+function build_index_grid(controls::ControlParameter...; offset::Bool=true, tspan::Tuple=(-Inf, Inf), subdivide::Int64=typemax(Int64))
+  ts = map(controls) do ci
+    clamp.(get_timegrid(ci), tspan...)
+  end
+  time_grid = vcat(reduce(vcat, ts), collect(tspan)) |> sort! |> unique! |> Base.Fix1(filter!, isfinite)
+  indices = zeros(Int64, length(ts), size(time_grid, 1) - 1)
+  for i in axes(indices, 1), j in axes(indices, 2)
+    indices[i, j] = clamp(
+      searchsortedlast(ts[i], time_grid[j]),
+      firstindex(ts[i]), lastindex(ts[i])
+    )
+  end
+  # Offset
+  if offset
+    for i in axes(indices, 1)
+      if i > 1
+        indices[i, :] .+= maximum(indices[i-1, :])
+      end
     end
-    time_grid = vcat(reduce(vcat, ts), collect(tspan)) |> sort! |> unique! |> Base.Fix1(filter!, isfinite)
-    indices = zeros(Int64, length(ts), size(time_grid, 1)-1)
-    for i in axes(indices, 1), j in axes(indices, 2)
-        indices[i,j] = clamp(
-            searchsortedlast(ts[i], time_grid[j]),
-            firstindex(ts[i]), lastindex(ts[i])
-        )
-    end
-    # Offset
-    if offset
-        for i in axes(indices, 1)
-            if i > 1
-                indices[i, :] .+= maximum(indices[i-1, :])
-            end
-        end
-    end
-    # Check the gridsize
-    N = size(indices,2)
-    if N > subdivide
-        ranges = get_subvector_indices(N, subdivide)
-        return Tuple(indices[:, i] for i in ranges)
-    end
-    return indices
+  end
+  # Check the gridsize
+  N = size(indices, 2)
+	# Normalize for the first index here 
+	indices .-= minimum(indices) - 1
+  if N > subdivide
+    ranges = get_subvector_indices(N, subdivide)
+    return Tuple(indices[:, i] for i in ranges)
+  end
+  return indices
 end
 
-function collect_tspans(controls::ControlParameter...; tspan=(-Inf, Inf), subdivide::Int64 = typemax(Int64))
-    ts = map(controls) do ci
-        clamp.(get_timegrid(ci), tspan...)
-    end
-    time_grid = vcat(reduce(vcat, ts), collect(tspan)) |> sort! |> unique! |> Base.Fix1(filter!, isfinite)
-    fullgrid = collect(ti for ti in zip(time_grid[1:end-1], time_grid[2:end]))
-    N = size(fullgrid,1)
-    if N > subdivide
-        ranges = get_subvector_indices(N, subdivide)
-        return Tuple(tuple(fullgrid[i]...) for i in ranges)
-    end
-    tuple(fullgrid...)
+function collect_tspans(controls::ControlParameter...; tspan=(-Inf, Inf), subdivide::Int64=typemax(Int64))
+  ts = map(controls) do ci
+    clamp.(get_timegrid(ci), tspan...)
+  end
+  time_grid = vcat(reduce(vcat, ts), collect(tspan)) |> sort! |> unique! |> Base.Fix1(filter!, isfinite)
+  fullgrid = collect(ti for ti in zip(time_grid[1:end-1], time_grid[2:end]))
+  N = size(fullgrid, 1)
+  if N > subdivide
+    ranges = get_subvector_indices(N, subdivide)
+    return Tuple(tuple(fullgrid[i]...) for i in ranges)
+  end
+  tuple(fullgrid...)
 end
 
-function collect_local_controls(rng, controls::ControlParameter...)
-    reduce(vcat, map(vec ∘ Base.Fix1(get_controls, rng), controls))
+function collect_local_controls(rng, controls::ControlParameter...; kwargs...)
+	reduce(vcat, map(controls) do control 
+		get_controls(rng, control; kwargs...)
+	end)
 end
 
 function collect_local_control_bounds(lower::Bool, controls::ControlParameter...)
-    reduce(vcat, map(vec ∘ (lower ? first : last) ∘ get_bounds, controls))
+  reduce(vcat, map(vec ∘ (lower ? first : last) ∘ get_bounds, controls))
 end
--- src/trajectory.jl
+++ src/trajectory.jl
@@ -8,7 +8,7 @@ $(FIELDS)
 # Note 
 If present, `shooting_points` contains a list of `Tuple`s `(timeseries_index, last_shooting_point)`.  
 """
-struct Trajectory{S, U, P, T, C}
+struct Trajectory{S, U, P, T}
 		"The symbolic system used for SymbolicIndexingInterface"
     sys::S
 		"The state trajectory"
@@ -17,8 +17,10 @@ struct Trajectory{S, U, P, T, C}
     p::P
 		"The timepoints"
     t::T
-		"The shooting points"
-		shooting_points::C
+		"The shooting values"
+		shooting::U
+		"The shooting indices"
+		shooting_indices::Vector{Int64}
 end
 
 SymbolicIndexingInterface.is_timeseries(::Type{<:Trajectory}) = Timeseries()
@@ -34,17 +36,15 @@ SymbolicIndexingInterface.current_time(fp::Trajectory) = fp.t
 utype(traj::Trajectory) = eltype(first(traj.u))
 ttype(traj::Trajectory) = eltype(traj.t)
 
-is_shooting_solution(::Trajectory) = false  
-is_shooting_solution(::Trajectory{<:Any,<:Any,<:Any,<:Any,Nothing}) = false  
+is_shooting_solution(traj::Trajectory) = isempty(traj.shooting_indices)
 
 """
 $(FUNCTIONNAME)
 Computes the shooting constraints over a given [Trajectory](@ref) if present. 
 """
-shooting_constraints(x::Trajectory{<:Any,<:Any,<:Any,<:Any,Nothing}) = utype(x)[]
 function shooting_constraints(traj::Trajectory)
-	(; u, shooting_points) = traj
-	reduce(vcat, map(shooting_points) do (idx, u_last) 
+	(; u, shooting, shooting_indices) = traj
+	reduce(vcat, map(zip(shooting_indices, shooting)) do (idx, u_last) 
 		vec(u_last .- u[idx])
 	end)
 end
@@ -53,11 +53,10 @@ end
 $(FUNCTIONNAME)
 Computes the shooting constraints over a given [Trajectory](@ref) if present in place. 
 """
-shooting_constraints!(res::AbstractArray, ::Trajectory{<:Any,<:Any,<:Any,<:Any,Nothing}) = nothing
 function shooting_constraints!(res::AbstractVector, traj::Trajectory)
-	(; u, shooting_points) = traj
+	(; u, shooting, shooting_indices) = traj
 	skip = prod(size(vec(u[1])))
-	foreach(enumerate(shooting_points)) do (i, (idx, u_last)) 
+	foreach(enumerate(zip(shooting_indices,shooting))) do (i, (idx, u_last)) 
 		res[(i-1)*skip+1:i*skip] .= vec(u_last .- u[idx])
 	end
 end
