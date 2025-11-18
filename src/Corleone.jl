978e1841bb07a9fc66d7a69f2f6e31b22384e96c
JuliusMartensen <julius.martensen@gmail.com>
2025-11-18 09:10:58 +0100

    Clean up deps

--- src/Corleone.jl
+++ src/Corleone.jl
@@ -6,7 +6,6 @@ using Random
 
 using RecursiveArrayTools
 using LinearAlgebra
-using ComponentArrays
 using SciMLBase
 using SciMLStructures
 using SymbolicIndexingInterface
@@ -14,7 +13,6 @@ using SymbolicIndexingInterface
 using OhMyThreads
 using Distributed
 
-using CommonSolve
 using ChainRulesCore
 
 using LuxCore
@@ -48,23 +46,11 @@ export SingleShootingLayer
 include("multiple_shooting.jl")
 export MultipleShootingLayer
 
-#include("augmentation.jl")
-
-#include("criteria.jl")
-#export ACriterion, DCriterion, ECriterion
-#export FisherACriterion, FisherDCriterion, FisherECriterion
 
 #include("node_initialization.jl")
 #export DefaultsInitialization, ConstantInitialization
 #export LinearInterpolationInitialization, ForwardSolveInitialization
 #export HybridInitialization, RandomInitialization, CustomInitialization
-
-#include("oed.jl")
-#export OEDLayer, get_sampling_constraint
-#include("multi_experiments.jl")
-#export MultiExperimentLayer
-#include("information_gain.jl")
-#export InformationGain
 
 #include("abstract.jl")
 
