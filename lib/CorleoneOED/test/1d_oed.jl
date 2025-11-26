9e1ed8ddba2b21aa57373a6693a1210af6a30712
JuliusMartensen <julius.martensen@gmail.com>
2025-11-26 13:24:25 +0100

    Update deps and project toml

--- lib/CorleoneOED/test/1d_oed.jl
+++ lib/CorleoneOED/test/1d_oed.jl
@@ -1,36 +1,34 @@
 using CorleoneOED
-
 using OrdinaryDiffEqTsit5
 using Test
-using Random
-using LuxCore
+using StableRNGs 
 
 using ComponentArrays
-
 using Optimization, OptimizationMOI, Ipopt
-using LinearAlgebra
-
-
-
 
 rng = Random.default_rng()
 
-function lin_dyn2( u, p, t)
-	return [ p[1]*u[1] + p[2]]
+function lin_dyn(u, p, t)
+  return [p[1] * u[1]]
 end
 
 u0 = [1.0]
-tspan = (0.,1.)
+tspan = (0.0, 1.0)
 p = [-2.0, 0.0]
 
 
-prob = ODEProblem(lin_dyn2,  u0, tspan, p, )
+prob = ODEProblem(lin_dyn2, u0, tspan, p,)
 
-ol = SingleShootingLayer(prob, Tsit5(), controls = [1 => ControlParameter([0., 0.1, 0.2]), 2 => ControlParameter([0., 0.5], )])
+ol = SingleShootingLayer(prob, Tsit5(), controls=[1 => ControlParameter([0.0, 0.1, 0.2]), 2 => ControlParameter([0.0, 0.5],)])
 
-oed = CorleoneOED.OEDLayer{false}(ol, params = [1,],)
+oed = CorleoneOED.OEDLayer{false}(ol, params=[1,],)
 
-oed = CorleoneOED.OEDLayer{true}(ol, params = [1,], measurements = [ControlParameter([0., 0.25, 0.5, 0.75]), ControlParameter([0.25, 0.75])], observed = (u,p,t) -> [u[1], u[1]^2] )
+oed = CorleoneOED.OEDLayer{true}(
+  ol,
+  params=[1, 2],
+  #measurements = [ControlParameter([0., 0.25, 0.5, 0.75], controls = ones(4)), ControlParameter([0.25, 0.75], controls = ones(2))], 
+	observed = (u,p,t) -> [u[1], (u[1]+p[2])^2] 
+)
 
 LuxCore.initialstates(rng, ol)
 
@@ -42,50 +40,50 @@ ps, st = LuxCore.setup(Random.default_rng(), oed)
 
 p = ComponentArray(ps)
 lb, ub = Corleone.get_bounds(ol)
-crit= ACriterion()
+crit = ACriterion()
 ACrit = crit(ol)
 
-sampling_cons = let ax = getaxes(p), sampling=Corleone.get_sampling_constraint(ol)
-    (res, p, ::Any) -> begin
-        ps = ComponentArray(p, ax)
-        res .= sampling(ps, nothing)
-    end
+sampling_cons = let ax = getaxes(p), sampling = Corleone.get_sampling_constraint(ol)
+  (res, p, ::Any) -> begin
+    ps = ComponentArray(p, ax)
+    res .= sampling(ps, nothing)
+  end
 end
 
 optfun = OptimizationFunction(
-    ACrit, AutoForwardDiff(), cons = sampling_cons
+  ACrit, AutoForwardDiff(), cons=sampling_cons
 )
 
 optprob = OptimizationProblem(
-    optfun, collect(p), lb = collect(lb), ub = collect(ub), lcons=zeros(1), ucons=[0.2]
+  optfun, collect(p), lb=collect(lb), ub=collect(ub), lcons=zeros(1), ucons=[0.2]
 )
 
 uopt = solve(optprob, Ipopt.Optimizer(),
-     tol = 1e-10,
-     hessian_approximation = "limited-memory",
-     max_iter = 300
+  tol=1e-10,
+  hessian_approximation="limited-memory",
+  max_iter=300
 )
 @testset "Convergence" begin
-    @test SciMLBase.successful_retcode(uopt)
+  @test SciMLBase.successful_retcode(uopt)
 end
 
 @testset "Information gain: Optimality criteria" begin
-    IG = InformationGain(ol, uopt.u)
+  IG = InformationGain(ol, uopt.u)
 
-    multiplier = uopt.original.inner.mult_g
+  multiplier = uopt.original.inner.mult_g
 
-    optimality = tr.(IG.global_information_gain[1])
+  optimality = tr.(IG.global_information_gain[1])
 
-    idxs_opt = optimality .> multiplier[1]
-    t_optimality = IG.t[idxs_opt]
-    @test !isempty(t_optimality)
-    @test all(0.4 .<= t_optimality .<= 0.6)
+  idxs_opt = optimality .> multiplier[1]
+  t_optimality = IG.t[idxs_opt]
+  @test !isempty(t_optimality)
+  @test all(0.4 .<= t_optimality .<= 0.6)
 
-    wopt = (uopt + zero(p)).controls
+  wopt = (uopt + zero(p)).controls
 
-    idxs_w = wopt .> 0.99
+  idxs_w = wopt .> 0.99
 
-    t_sampling = ol.layer.controls[1].t[idxs_w]
+  t_sampling = ol.layer.controls[1].t[idxs_w]
 
-    @test all( 0.4 .<= t_sampling .<= 0.6)
+  @test all(0.4 .<= t_sampling .<= 0.6)
 end
