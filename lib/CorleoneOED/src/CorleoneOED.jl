c2b3e1c280c5844baaced4edae97723f4871c2a3
JuliusMartensen <julius.martensen@gmail.com>
2025-11-27 18:40:19 +0100

    Clean and comment

--- lib/CorleoneOED/src/CorleoneOED.jl
+++ lib/CorleoneOED/src/CorleoneOED.jl
@@ -12,13 +12,19 @@ using SciMLBase
 using DocStringExtensions
 using LinearAlgebra
 
+# TODO Clean and more tests (also for LV)
 include("augmentation.jl")
 
+# TODO Docs
 include("oed.jl")
 export OEDLayer
 export fisher_information, observed_equations, sensitivities
 export local_information_gain, global_information_gain
 
+# TODO 
+# Dispatch for optimality(crit, oed, x, ps, st) 
+# --> Compute optimality condition for specific crit 
+# e.g. tr(\Pi) for the ACriterion
 include("criteria.jl")
 export ACriterion, DCriterion, ECriterion
 export FisherACriterion, FisherECriterion, FisherDCriterion
