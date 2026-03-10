45359dc59af2f79b9fc26c0fe0accbc8091e463e
JuliusMartensen <julius.martensen@gmail.com>
2026-03-10 19:03:17 +0100

    Update plots

--- ext/CorleoneMakieExtension.jl
+++ ext/CorleoneMakieExtension.jl
@@ -1,5 +1,6 @@
 module CorleoneMakieExtension
 using Corleone
+using SymbolicIndexingInterface
 using Makie
 
 Makie.plottype(sol::Trajectory) = Makie.Lines
@@ -8,34 +9,56 @@ function Makie.used_attributes(::Type{<:Plot}, sol::Trajectory)
     return (:vars, :idxs)
 end
 
+maybevec(x::AbstractArray) = eachrow(reduce(hcat, x))
+maybevec(x) = x
+
 function Makie.convert_arguments(
         PT::Type{<:Plot},
         sol::Trajectory;
-        vars = nothing,
-        idxs = nothing
+	idxs::AbstractVector{<:Int} = Int64[], 
+		vars::AbstractVector = [], 
+		kwargs...
     )
-
-    if !isnothing(vars)
-        (!isnothing(idxs)) && error("Can't simultaneously provide vars and idxs!")
-        idxs = vars
+	if !isempty(idxs)  
+		append!(
+			vars,
+			variable_symbols(sol)[idxs]
+		)
     end
-
-    idxs = isnothing(idxs) ? eachindex(sol.u[1]) : idxs
-    idxs = eltype(idxs) == Symbol ? [sol.sys.variables[i] for i in idxs] : idxs
-
-    plot_vecs = reduce(hcat, sol.u)[idxs, :]
+	if isempty(vars)
+		for v in variable_symbols(sol) 
+			push!(vars, variable_index(sol, v)) 
+		end
+	end
+	ts = [] 
+	xs = [] 
+	labels = String[] 
+	foreach(vars) do var 
+		if is_timeseries_parameter(sol, var) 
+			x_current = maybevec(getp(sol, var)(sol)) 
+			append!(xs, x_current) 
+			for i in eachindex(x_current)
+				push!(ts, sol.controls.collection[1].t)
+				push!(labels, string(getindex(var, i)))
+			end
+		else 
+			x_current = maybevec(getsym(sol, var)(sol)) 
+			append!(xs, x_current) 
+			for i in eachindex(x_current)
+				push!(ts, sol.t)
+				push!(labels, string(getindex(var, i)))
+			end
+		end
+	end
 
     plot_type_sym = Makie.plotsym(PT)
-
-    inv_sys_variables = Dict(value => key for (key, value) in sol.sys.variables)
-    labels = string.([inv_sys_variables[i] for i in idxs])
-
+	
     return map(
-        (x, y, label, i) -> PlotSpec(plot_type_sym, Point2f.(x, y); label, color = Cycled(i)),
-        [sol.t for _ in eachindex(idxs)],
-        eachrow(plot_vecs),
+		(x, y, label, i) -> PlotSpec(plot_type_sym, Point2f.(x, y); label, color = Cycled(i), kwargs...),
+        ts,
+        xs,
         labels,
-        eachindex(idxs)
+        eachindex(labels)
     )
 end
 end
