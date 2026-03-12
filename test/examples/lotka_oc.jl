4908fc287fc68df19be2b9069f3f78521c0988f9
JuliusMartensen <julius.martensen@gmail.com>
2026-03-12 12:26:24 +0100

    Adapt lotka optimal control

--- test/examples/lotka_oc.jl
+++ test/examples/lotka_oc.jl
@@ -23,72 +23,94 @@ end
 tspan = (0.0, 12.0)
 u0 = [0.5, 0.7, 0.0]
 p0 = [0.0, 1.0, 1.0]
-prob = ODEProblem(lotka_dynamics!, u0, tspan, p0; abstol = 1.0e-8, reltol = 1.0e-6)
+prob = ODEProblem(
+    ODEFunction(lotka_dynamics!, sys=SymbolCache([:x, :y, :c], [:u, :α, :β], :t)),
+    u0,
+    tspan,
+    p0;
+    abstol=1.0e-8,
+    reltol=1.0e-6,
+)
 
 cgrid = collect(0.0:0.1:11.9)
 N = length(cgrid)
-control = ControlParameter(
-    cgrid, name = :fishing, bounds = (0.0, 1.0), controls = zeros(N)
+controls = (
+    ControlParameter(
+        cgrid;
+        name=:u,
+        bounds=t -> (zero(t), zero(t) .+ 1),
+        controls=(rng, t) -> zeros(eltype(t), length(t)),
+    ),
+    FixedControlParameter(; name=:α, controls=(rng, t) -> [1.0]),
+    FixedControlParameter(; name=:β, controls=(rng, t) -> [1.0]),
 )
 
-layer = SingleShootingLayer(prob, Tsit5(); controls = (1 => control,), bounds_p = ([1.0, 1.0], [1.0, 1.0]))
+layer = SingleShootingLayer(prob, controls...; algorithm=Tsit5(), quadrature_indices=[3])
 
 ps, st = LuxCore.setup(rng, layer)
 
 sol, _ = layer(nothing, ps, st)
 
 @test sol.t == getsym(sol, :t)(sol)
-@test sol.p[1] == getsym(sol, :p₁)(sol)
-@test sol.p[2] == getsym(sol, :p₂)(sol)
+@test all(sol.p[2] .== getsym(sol, :α)(sol))
+@test all(sol.p[3] .== getsym(sol, :β)(sol))
+@test ps.controls.u == sol.ps[:u]
 
 x = reduce(hcat, sol.u)
 
-for (i, sym) in enumerate((:x₁, :x₂, :x₃, :fishing))
+for (i, sym) in enumerate((:x, :y, :c))
     getter = getsym(sol, sym)
     @test getter(sol) == x[i, :]
 end
 
-@test_nowarn @inferred layer(nothing, ps, st)
+@test_nowarn @inferred first(layer(nothing, ps, st))
 
 @test allunique(sol.t)
 @test LuxCore.parameterlength(layer) == N + 2
 
 
 for AD in (AutoForwardDiff(), AutoReverseDiff(), AutoZygote())
-    prob = ODEProblem(lotka_dynamics!, u0, tspan, p0; abstol = 1.0e-8, reltol = 1.0e-6, sensealg = AD == AutoZygote() ? ForwardDiffSensitivity() : SciMLBase.NoAD())
-
-    cgrid = collect(0.0:0.1:11.9)
-    N = length(cgrid)
-    control = ControlParameter(
-        cgrid, name = :fishing, bounds = (0.0, 1.0), controls = zeros(N)
+    prob = ODEProblem(
+        ODEFunction(lotka_dynamics!, sys=SymbolCache([:x, :y, :c], [:u, :α, :β], :t)),
+        u0,
+        tspan,
+        p0;
+        abstol=1.0e-8,
+        reltol=1.0e-6,
+        sensealg=AD == AutoZygote() ? ForwardDiffSensitivity() : SciMLBase.NoAD(),
     )
 
-    layer = SingleShootingLayer(prob, Tsit5(); controls = (1 => control,), bounds_p = ([1.0, 1.0], [1.0, 1.0]))
+    layer = SingleShootingLayer(prob, controls...; algorithm=Tsit5(), quadrature_indices=[3])
 
     ps, st = LuxCore.setup(rng, layer)
 
     p = ComponentArray(ps)
-    lb, ub = Corleone.get_bounds(layer)
 
-    @test lb.p == ub.p == p0[2:end]
-    @test lb.controls == zeros(N)
-    @test ub.controls == ones(N)
-    @test size(p, 1) == LuxCore.parameterlength(layer)
+    loss = let layer = layer, ax = getaxes(p)
+        (p, st) -> begin
+            traj, _ = layer(nothing, ComponentArray(p, ax), st)
+            traj[:c][end]
+        end
+    end
+    #@test size(p, 1) == LuxCore.parameterlength(layer)
+    #optprob = OptimizationProblem(layer, AD, Val(:ComponentArrays), loss = :c)
+    optfun = OptimizationFunction(loss, AD,)
+    optprob = OptimizationProblem(optfun, collect(p), st;
+        lb=zero(p), ub=zero(p) .+ 1
+    )
 
-    optprob = OptimizationProblem(layer, AD, Val(:ComponentArrays), loss = :x₃)
-
-    @test isapprox(optprob.f(optprob.u0, optprob.p), 6.062277454291031, atol = 1.0e-4)
+    @test isapprox(optprob.f(optprob.u0, optprob.p), 6.062277454291031, atol=1.0e-4)
 
     sol = solve(
-        optprob, Ipopt.Optimizer(), max_iter = 1000, tol = 5.0e-6,
-        hessian_approximation = "limited-memory"
+        optprob, Ipopt.Optimizer(), max_iter=1000, tol=5.0e-6,
+        hessian_approximation="limited-memory"
     )
 
     @test SciMLBase.successful_retcode(sol)
-    @test isapprox(sol.objective, 1.344336, atol = 1.0e-4)
+    @test isapprox(sol.objective, 1.344336, atol=1.0e-4)
 
     p_opt = sol.u .+ zero(p)
 
-    @test isempty(p_opt.u0)
-    @test p_opt.p == p0[2:end]
+    @test isempty(p_opt.initial_conditions)
+    @test length(p_opt.controls.u) == N
 end
