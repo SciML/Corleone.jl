c3fa160f85c6c881acbf41922df97b2aedab5fd7
JuliusMartensen <julius.martensen@gmail.com>
2026-03-17 09:36:23 +0100

    Format

--- ext/CorleoneComponentArraysExtension.jl
+++ ext/CorleoneComponentArraysExtension.jl
@@ -2,18 +2,18 @@ module CorleoneComponentArraysExtension
 using Corleone
 using ComponentArrays
 
-Corleone.to_vec(::Val{:ComponentArrays}, u) = begin 
-	collect(ComponentArray(u))
+Corleone.to_vec(::Val{:ComponentArrays}, u) = begin
+    collect(ComponentArray(u))
 end
 
-function Corleone.WrappedFunction(::Val{:ComponentArrays}, f, p, st; kwargs...) 
+function Corleone.WrappedFunction(::Val{:ComponentArrays}, f, p, st; kwargs...)
     u0 = ComponentVector(p)
-	pre = let ax = getaxes(u0)
-		(p) -> ComponentArray(p, ax) 	
-	end
-	Corleone.WrappedFunction{
-		typeof(f), typeof(pre)
-	}(f, pre)
+    pre = let ax = getaxes(u0)
+        (p) -> ComponentArray(p, ax)
+    end
+    return Corleone.WrappedFunction{
+        typeof(f), typeof(pre),
+    }(f, pre)
 end
 
 end
