aac99c6574141ddc709332d9e808331d439b0f5a
Christoph Plate <christoph.plate@ovgu.de>
2026-05-11 11:52:07 +0200

    integer weights in multiexperiments

--- lib/CorleoneOED/ext/CorleoneOEDOptimizationExtension.jl
+++ lib/CorleoneOED/ext/CorleoneOEDOptimizationExtension.jl
@@ -327,14 +327,21 @@ function get_integrality(layer::OEDLayer{<:Any, true, <:Any, <:SingleShootingLay
     return int_u0
 end
 
-function get_integrality(layer::MultiExperimentLayer{<:Any, false, <:Any, <:SingleShootingLayer}, integer::Bool, ps::ComponentArray, st)
+function get_integrality(layer::MultiExperimentLayer{<:Any, <:Any, false}, integer::Bool, ps::ComponentArray, st::NamedTuple{fields}) where {fields}
     int_u0 = Bool.(ps * 0)
     !integer &&  return int_u0
+    for field in fields
+        int_u0[field] = get_integrality(layer.layers, integer, getproperty(int_u0, field), getproperty(st, field))
+    end
     return int_u0
 end
-function get_integrality(layer::MultiExperimentLayer{<:Any, false, <:Any, <:MultipleShootingLayer}, integer::Bool, ps::ComponentArray, st)
+
+function get_integrality(layer::MultiExperimentLayer{<:Any, false, true}, integer::Bool, ps::ComponentArray, st)
     int_u0 = Bool.(ps * 0)
     !integer &&  return int_u0
+    for (i,field) in enumerate(fields)
+        int_u0[field] = get_integrality(layer.layers[i], integer, getproperty(int_u0, field), getproperty(st, field))
+    end
     return int_u0
 end
 
