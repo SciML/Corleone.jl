922e0290a6c6cf0d2b07f166efbe75efac934bbe
Carl Julius Martensen <julius.martensen@gmail.com>
2026-07-15 08:20:14 +0200

    Prune commits

--- src/Corleone.jl
+++ src/Corleone.jl
@@ -19,8 +19,6 @@ using ChainRulesCore
 using LuxCore
 using Functors
 
-#include("core/Core.jl")
-
 include("solutions/Solutions.jl")
 
 include("layers/Layers.jl")
@@ -30,26 +28,4 @@ include("layers/Layers.jl")
 shooting_constraints(traj::Solutions.Trajectory) = Solutions.shooting_constraints(traj)
 shooting_constraints!(res::AbstractVector, traj::Solutions.Trajectory) = Solutions.shooting_constraints!(res, traj)
 
-#=
-
-include("trajectory.jl")
-export Trajectory
-
-include("local_controls.jl")
-export ControlParameter
-
-include("single_shooting.jl")
-export SingleShootingLayer
-include("multiple_shooting.jl")
-export MultipleShootingLayer
-export default_initialization
-include("node_initialization.jl")
-export random_initialization, forward_initialization, linear_initialization
-export custom_initialization, constant_initialization, hybrid_initialization
-
-abstract type AbstractCorleoneFunctionWrapper end
-
-include("dynprob.jl")
-export CorleoneDynamicOptProblem
-=#
 end
--- src/core/utils.jl
+++
@@ -1,8 +0,0 @@
-# For evaluation
-mythreadmap(::EnsembleSerial, args...) = map(args...)
-mythreadmap(::EnsembleThreads, args...) = tmap(args...)
-mythreadmap(::EnsembleDistributed, args...) = pmap(args...)
-
-# Bounds 
-default_init(rng, t, bounds) = zeros(eltype(t), size(t))
-default_bounds(t::AbstractVector{T}) where {T <: Real} = (fill(typemin(T), size(t)), fill(typemax(T), size(t)))
